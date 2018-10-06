523ef15f846b0d94b29806e6235c479ee73a88fa
Anthony Willmore <Willmore976@users.noreply.github.com>
2018-10-05 18:03:54 -0700

    Delete partinfo.sql

--- partinfo.sql
+++
@@ -1,4 +0,0 @@
-
-DEFINE v_part_desc = 'Big Filter'                                               
-DEFINE v_part_qty = '1110'                                                      
-                                                                                
